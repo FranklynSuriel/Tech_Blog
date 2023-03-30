d177dde4ae467c8453ca49d077a3e7e509146254
Franksu <fsuriel@gmail.com>
2023-03-30 13:43:49 -0400

    User can deslete post

--- controllers/apiRoutes/userRoutes.js
+++ controllers/apiRoutes/userRoutes.js
@@ -1,6 +1,7 @@
 // import packages
 const router = require('express').Router();
 const { User, Post, Comments } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 router.post('/login', async (req, res) => {
     try {
@@ -84,4 +85,25 @@ router.post('/comment', async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if(!postData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+
+        res.status(200).json(postData);
+
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
